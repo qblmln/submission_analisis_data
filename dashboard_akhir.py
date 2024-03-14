bcd1fea5c561a8099dda23d07fde670ecb60c3b2
qblmln <104448867+qblmln@users.noreply.github.com>
2024-03-14 13:51:17 +0700

    Update dashboard_akhir.py

--- dashboard_akhir.py
+++ dashboard_akhir.py
@@ -71,7 +71,7 @@ def create_rfm_df(df):
     return rfm_df
 
 # Load cleaned data
-all_df = pd.read_csv("https://github.com/qblmln/submission_analisis_data/blob/main/all_data.csv")
+all_df = pd.read_csv("https://raw.githubusercontent.com/qblmln/submission_analisis_data/main/all_data.csv")
 
 datetime_columns = ["order_estimated_delivery_date"]
 
